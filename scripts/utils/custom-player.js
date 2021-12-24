48dcb81f75f39def0b11a88ec9c33cece23e9f92
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-12-24 13:05:06 +0100

    feat: reward and forward buttons operational on the videos

--- scripts/utils/custom-player.js
+++ scripts/utils/custom-player.js
@@ -1,20 +1,20 @@
 const manageMediaControls = () => {
-  const video = document.querySelector('video.video');
-  
-  const controls = document.querySelector('.controls');
+  const video = document.querySelector("video.video");
 
-  const play = document.querySelector('.playPauseButton');
-  const stop = document.querySelector('.stopButton');
-  const reward = document.querySelector('.rewardButton');
-  const forward = document.querySelector('.forwardButton');
+  const controls = document.querySelector(".controls");
 
-  const timerWrapper = document.querySelector('.timer');
-  const timer = document.querySelector('.timer span');
-  const timerBar = document.querySelector('.timer div');
+  const play = document.querySelector(".playPauseButton");
+  const stop = document.querySelector(".stopButton");
+  const reward = document.querySelector(".rewardButton");
+  const forward = document.querySelector(".forwardButton");
+
+  const timerWrapper = document.querySelector(".timer");
+  const timer = document.querySelector(".timer span");
+  const timerBar = document.querySelector(".timer div");
 
   // remove the default browser controls on the video, and make our custom controls visible.
-  video.removeAttribute('controls');
-  controls.style.visibility = 'visible';
+  video.removeAttribute("controls");
+  controls.style.visibility = "visible";
 
   // =======================================
   // === Play and pause of the the video ===
@@ -22,17 +22,17 @@ const manageMediaControls = () => {
   const playPauseVideo = () => {
     // Tells whether the media element is paused. Return true if is paused. "u" is the icon "pause" of "heydings font"
     // The "play" method launch the video
-    if(video.paused) {
-      play.setAttribute('data-icon','u');
+    if (video.paused) {
+      play.setAttribute("data-icon", "u");
       video.play();
     } else {
       // On the second click, the button will be alternated again
-      play.setAttribute('data-icon', 'P');
+      play.setAttribute("data-icon", "P");
       video.pause();
     }
-  }
+  };
 
-  play.addEventListener('click', playPauseVideo);
+  play.addEventListener("click", playPauseVideo);
 
   // =========================
   // === Stop of the video ===
@@ -42,12 +42,12 @@ const manageMediaControls = () => {
   const stopVideo = () => {
     video.pause();
     video.currentTime = 0;
-    play.setAttribute('data-icon', 'P');
-  }
+    play.setAttribute("data-icon", "P");
+  };
 
-  stop.addEventListener('click', stopVideo);
-  video.addEventListener('ended', stopVideo);
-  video.addEventListener('click', playPauseVideo);
+  stop.addEventListener("click", stopVideo);
+  video.addEventListener("ended", stopVideo);
+  video.addEventListener("click", playPauseVideo);
 
   // ===========================
   // === Update elapsed time ===
@@ -58,13 +58,13 @@ const manageMediaControls = () => {
     let minuteValue;
     let secondValue;
 
-    if(minutes < 10) {
+    if (minutes < 10) {
       minuteValue = `0${minutes}`;
     } else {
       minuteValue = minutes;
     }
 
-    if(seconds < 10) {
+    if (seconds < 10) {
       secondValue = `0${seconds}`;
     } else {
       secondValue = seconds;
@@ -73,10 +73,78 @@ const manageMediaControls = () => {
     let videoTime = `${minuteValue} : ${secondValue}`;
     timer.textContent = videoTime;
 
-    let progressBarLength = timerWrapper.clientWidth * (video.currentTime / video.duration);
+    let progressBarLength =
+      timerWrapper.clientWidth * (video.currentTime / video.duration);
     timerBar.style.width = `${progressBarLength}px`;
-}
+  };
 
-video.addEventListener('timeupdate', setTime);
+  video.addEventListener("timeupdate", setTime);
 
-}
+  // === Backward and forward
+
+  let intervalForward;
+  let intervalReward;
+
+  const videoBackward = () => {
+    // reset class active and interval on forward to avoid conflict if reward is clicked
+    clearInterval(intervalForward);
+    forward.classList.remove("active");
+
+    if (reward.classList.contains("active")) {
+      // if 'active' already exists so the reward button has already been pressed
+      // we reset the interval defined on the button when pressed, remove the 'active' class and launch the video
+      reward.classList.remove("active");
+      clearInterval(intervalReward);
+      video.play();
+    } else {
+      // else we add the 'active' class and pause the video.
+      reward.classList.add("active");
+      video.pause();
+      // windBackward is called every 800 ms
+      intervalReward = setInterval(windBackward, 800);
+    }
+  };
+
+  const videoForward = () => {
+    clearInterval(intervalReward);
+    reward.classList.remove("active");
+
+    if (forward.classList.contains("active")) {
+      forward.classList.remove("active");
+      clearInterval(intervalForward);
+      video.play();
+    } else {
+      forward.classList.add("active");
+      video.pause();
+      intervalForward = setInterval(windForward, 800);
+    }
+  };
+
+  // executed every 800ms
+  function windBackward() {
+    // if the current playing time of the video is <= 3s form the begining
+    // we stop the video, remove the 'active' class and reset the intervalReward to avoid infinite rewind
+    if (video.currentTime <= 3) {
+      reward.classList.remove("active");
+      clearInterval(intervalReward);
+      stopVideo();
+    } else {
+      // we go back 3 seconds
+      video.currentTime -= 3;
+    }
+  }
+
+  function windForward() {
+    // if the current playing time of the video is <= 3s form the end
+    if (video.currentTime >= video.duration - 3) {
+      forward.classList.remove("active");
+      clearInterval(intervalForward);
+      stopVideo();
+    } else {
+      video.currentTime += 3;
+    }
+  }
+
+  reward.addEventListener("click", videoBackward);
+  forward.addEventListener("click", videoForward);
+};
