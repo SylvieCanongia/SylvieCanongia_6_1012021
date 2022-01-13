c01f4247d9de7233e27927f3f756f38f479aa76a
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-01-13 22:58:56 +0100

    feat: keyboard focus on the first focusable element on opening the contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,214 +1,276 @@
-// DOM Elements
-const modalbg = document.querySelector(".contactModal__container");
-const modalBtn = document.querySelectorAll(".contactButton");
-const formData = document.querySelectorAll(".formData");
-const closeModalCrossElement = document.querySelector(".close");
-// get the form element
-const forms = document.querySelectorAll("form[data-form]");
-const formElement = document.querySelector("form[data-form]");
-const modalBodyElement = document.querySelector(".contactModal__body");
+document.addEventListener('DOMContentLoaded', () => { 
 
-// Managing of the errors
-let error;
-let errors;
+  // DOM Elements
+  const modalbg = document.querySelector(".contactModal__container");
+  
+  const formData = document.querySelectorAll(".formData");
+  const closeModalCrossElement = document.querySelector(".close");
+  // get the form element
+  const forms = document.querySelectorAll("form[data-form]");
+  const formElement = document.querySelector("form[data-form]");
+  const modalBodyElement = document.querySelector(".contactModal__body");
 
-// ==============================================
-// === Modal subscription opening and closing ===
-// ==============================================
+  // Managing of the errors
+  let error;
+  let errors;
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+  // ==================================
+  // === CONTROLS AND ACCESSIBILITY ===
+  // ==================================
 
-// launch modal form
-function launchModal() {
-  // display the form content in case
-  // it is not the first time the button is clicked
-  formElement.style.display = "block";
-  modalbg.style.display = "block";
-}
+  // Get all the triggers of type : aria-haspopup="dialog"
+  const triggers = document.querySelectorAll('[aria-haspopup="dialog"]');
+  const mainElement = document.querySelector('.main');
 
-// === Close the modal by click on the cross ===
+  // array of all the focusable elements of the modal window
+  const focusableElementsArray = [
+    '[href]',
+    'button:not([disabled])',
+    'input:not([disabled])',
+    'select:not([disabled])',
+    'textarea:not([disabled])',
+    '[tabindex]:not([tabindex="-1"])',
+  ];
 
-// function to close the modal
-const closeModal = () => {
-  // Removes the error messages
-  formData.forEach(formD => {
-    formD.setAttribute("data-error-visible", "false");
-    formD.setAttribute("data-error", "");
-  });
-  error = "";
-  // Close the modal
-  modalbg.style.display = "none";
-}
+  // ==============================================
+  // === Modal contact opening and closing ===
+  // ==============================================
+ 
 
-// listen the event click on the cross
-closeModalCrossElement.addEventListener("click", closeModal);
+  const openModal = (dialog) => {
+    // Select all the focusable elements of the modal
+    const focusableElements = dialog.querySelectorAll(focusableElementsArray);
+    const firstFocusableElement = focusableElements[0];
+    console.log(focusableElements[0])
+    
+     // Activate modal and disable main element when modal window opens
+    dialog.setAttribute('aria-hidden', false);
+    mainElement.setAttribute('aria-hidden', true);
 
-// =======================
-// === Form validation ===
-// =======================
-
-// Check if form exist
-if (forms.length > 0) {
-  // Loop on all elements
-  for (let form of forms) {
-    // Get all inputs that have to be validated (have data-validate attribute)
-    const inputs = form.querySelectorAll("[data-validate]");
-
-    // Listen the form submit event and submit the form
-    // bind allow to pass all inputs as argument
-    form.addEventListener("submit", submitForm.bind(form, inputs));
-  }
-}
-
-
-// ======================
-// === VALIDATE INPUT ===
-// ======================
-
-// Validate input
-function validateInput(input) {
-  // get the value and formData element for assigning error message
-  // (via CSS pseudo-elements)
-  const value = input.value;
-  const formDataElement = input.closest("[data-formData]");
-  // Declare error variable for displaying error messages and assign null by default
-  error = null;
-
-  // Check if : -> if the input is not radio or checkbox
-  // -> and input has data-required attribute
-  //  -> and the value is empty and the value has a required minlength
-  // -> the input value is < to the minlength
-  if (
-    input.type !== "radio" &&
-    input.type !== "checkbox" &&
-    input.dataset.required !== undefined &&
-    input.dataset.minlength !== undefined &&
-    value.length < input.dataset.minlength
-  ) {
-    // Set an error message to the data-error attribute for display to the user
-    formDataElement.setAttribute(
-      "data-error",
-      `Ce champ est requis. Veuillez entrer au moins ${input.dataset.minlength} caractères.`
-    );
-    error = formDataElement.dataset.error;
-  }
-
-  // Check if input has data-email attribute and if email is not valid with validateEmail function
-  if (input.dataset.email !== undefined && !validateEmail(value)) {
-    formDataElement.setAttribute(
-      "data-error",
-      "Ce champ est requis. Veuillez entrer une adresse email valide."
-    );
-    error = formDataElement.dataset.error;
-  }
-
-  // Validate email using a regex
-  function validateEmail(email) {
-    const regexMail =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regexMail.test(String(email).toLowerCase());
-  }
-
-  // If there is no error, remove message from error element and so data-error attribute
-  // and set data-error-visible attribute to false
-  if (!error) {
-    formDataElement.setAttribute("data-error-visible", "false");
-    error = "";
-    formDataElement.setAttribute("data-error", "");
-  } else {
-    formDataElement.setAttribute("data-error-visible", "true");
-  }
-
-  return error;
-}
-
-// ============================
-// ======== SUBMITFORM ========
-// ============================
-
-// submit form on submit button click
-// all inputs are passed as argument with bind to loop through inputs
-// and call validateInput on each input element
-function submitForm(inputs, event) {
-  event.preventDefault();
-  errors = [];
-
-  inputs.forEach((input) => {
-    const error = validateInput(input);
-    if (error) {
-      errors.push(error);
+    // return if no focusable element
+    if(!firstFocusableElement) {
+      return;
     }
+
+    // Set focus on the first focusable element with a delay
+    // to focus after the open modal animation
+    window.setTimeout(() => {
+      firstFocusableElement.focus();
+    }, 300);
+  };
+
+  const closeModal = (dialog) => {
+    dialog.setAttribute('aria-hidden', true);
+    mainElement.setAttribute('aria-hidden', false);
+    
+    // Removes the error messages
+    formData.forEach(formD => {
+      formD.setAttribute("data-error-visible", "false");
+      formD.setAttribute("data-error", "");
+    });
+    error = "";
+  }
+
+  triggers.forEach((trigger) => {
+    // Get the modal linked to the trigger via aria-controls attribute
+    const dialog = document.getElementById(trigger.getAttribute('aria-controls'));
+    
+    // Get all the triggers for closing the modal via dta attribute
+    const closeTriggers = dialog.querySelectorAll('[data-close]');
+
+    // Open the dialog modal via event 'click'
+    trigger.addEventListener("click", (event) => {
+      event.preventDefault();
+      openModal(dialog);
+    });
+
+    // CLOSE THE DIALOG MODAL
+
+    closeTriggers.forEach((closeTrigger) => {
+      // Get the modal matching with the trigger via the id
+      const dialogModalToClose = document.getElementById(closeTrigger.dataset.close);
+      
+      //  Close by clic on the trigger
+      closeTrigger.addEventListener('click', (event) => {
+        closeModal(dialogModalToClose);
+      });
+    });
+
+    // Close the modal by click on modal background
+    window.addEventListener('click', (event) => {
+      if(event.target === dialog) {
+        closeModal(dialog);
+      }
+    });
   });
 
-  // Check if errors array is empty and only in that case, form is submited
-  if (errors.length === 0) {
-    logDatas();
-    // reset the form
-    formElement.reset();
-    confirmSubmission();
+  // =======================
+  // === Form validation ===
+  // =======================
+
+  // Check if form exist
+  if (forms.length > 0) {
+    // Loop on all elements
+    for (let form of forms) {
+      // Get all inputs that have to be validated (have data-validate attribute)
+      const inputs = form.querySelectorAll("[data-validate]");
+
+      // Listen the form submit event and submit the form
+      // bind allow to pass all inputs as argument
+      form.addEventListener("submit", submitForm.bind(form, inputs));
+    }
   }
-}
 
-// =========================
-// === CONFIRM SUBMISSION ===
-// ==========================
 
-function confirmSubmission() {
-  // hide the form content
-  formElement.style.display = "none";
+  // ======================
+  // === VALIDATE INPUT ===
+  // ======================
 
-  // Display a message after form validation success
-  // 1. Create a div element
-  const divElement = document.createElement("div");
-  divElement.className = "modal-confirm";
+  // Validate input
+  function validateInput(input) {
+    // get the value and formData element for assigning error message
+    // (via CSS pseudo-elements)
+    const value = input.value;
+    const formDataElement = input.closest("[data-formData]");
+    // Declare error variable for displaying error messages and assign null by default
+    error = null;
 
-  // 2. Put a p element into the div element
-  const pElement = document.createElement("p");
-  pElement.textContent = "Merci pour votre message. Je vous répondrai dans les meilleurs délais.";
+    // Check if : -> if the input is not radio or checkbox
+    // -> and input has data-required attribute
+    //  -> and the value is empty and the value has a required minlength
+    // -> the input value is < to the minlength
+    if (
+      input.type !== "radio" &&
+      input.type !== "checkbox" &&
+      input.dataset.required !== undefined &&
+      input.dataset.minlength !== undefined &&
+      value.length < input.dataset.minlength
+    ) {
+      // Set an error message to the data-error attribute for display to the user
+      formDataElement.setAttribute(
+        "data-error",
+        `Ce champ est requis. Veuillez entrer au moins ${input.dataset.minlength} caractères.`
+      );
+      error = formDataElement.dataset.error;
+    }
 
-  divElement.appendChild(pElement);
+    // Check if input has data-email attribute and if email is not valid with validateEmail function
+    if (input.dataset.email !== undefined && !validateEmail(value)) {
+      formDataElement.setAttribute(
+        "data-error",
+        "Ce champ est requis. Veuillez entrer une adresse email valide."
+      );
+      error = formDataElement.dataset.error;
+    }
 
-  // Add a button for closing the confirmation modal
-  const buttonElement = document.createElement("input");
-  buttonElement.classList.add("btn-submit", "btn-close-modal");
-  buttonElement.setAttribute("type", "button");
-  buttonElement.setAttribute("value", "Fermer");
+    // Validate email using a regex
+    function validateEmail(email) {
+      const regexMail =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regexMail.test(String(email).toLowerCase());
+    }
 
-  divElement.appendChild(buttonElement);
+    // If there is no error, remove message from error element and so data-error attribute
+    // and set data-error-visible attribute to false
+    if (!error) {
+      formDataElement.setAttribute("data-error-visible", "false");
+      error = "";
+      formDataElement.setAttribute("data-error", "");
+    } else {
+      formDataElement.setAttribute("data-error-visible", "true");
+    }
 
-  // Put the div element into the modal body
-  modalBodyElement.appendChild(divElement);
+    return error;
+  }
 
-  closeModalCrossElement.addEventListener("click", closeConfirmModal);
+  // ============================
+  // ======== SUBMITFORM ========
+  // ============================
 
-  const btnCloseModalElement = document.querySelector(".btn-close-modal");
-  btnCloseModalElement.addEventListener("click", closeConfirmModal);
-}
+  // submit form on submit button click
+  // all inputs are passed as argument with bind to loop through inputs
+  // and call validateInput on each input element
+  function submitForm(inputs, event) {
+    event.preventDefault();
+    errors = [];
 
-// ==================================
-// === Modal confirmation closing ===
-// ==================================
+    inputs.forEach((input) => {
+      const error = validateInput(input);
+      if (error) {
+        errors.push(error);
+      }
+    });
 
-function closeConfirmModal() {
-  // Select and remove the p Element
-  modalBodyElement.querySelector("p").remove();
-  // Select and remove the button Element
-  modalBodyElement.querySelector(".btn-close-modal").remove();
-  // Select and remove the div Element
-  modalBodyElement.querySelector(".modal-confirm").remove();
-  // close the modal
-  modalbg.style.display = "none";
-}
+    // Check if errors array is empty and only in that case, form is submited
+    if (errors.length === 0) {
+      logDatas();
+      // reset the form
+      formElement.reset();
+      confirmSubmission();
+    }
+  }
 
-// ========================================
-// === Get the form datas ===
-// === and display them in the console ====
-// ========================================
+  // =========================
+  // === CONFIRM SUBMISSION ===
+  // ==========================
 
-function logDatas() {
-  const datas = document.querySelectorAll('[data-validate]');
-  datas.forEach(data => {
-    console.log(`${data.name} : ${data.value}`);
-  })
-}
+  function confirmSubmission() {
+    // hide the form content
+    formElement.style.display = "none";
+
+    // Display a message after form validation success
+    // 1. Create a div element
+    const divElement = document.createElement("div");
+    divElement.className = "modal-confirm";
+
+    // 2. Put a p element into the div element
+    const pElement = document.createElement("p");
+    pElement.textContent = "Merci pour votre message. Je vous répondrai dans les meilleurs délais.";
+
+    divElement.appendChild(pElement);
+
+    // Add a button for closing the confirmation modal
+    const buttonElement = document.createElement("input");
+    buttonElement.classList.add("btn-submit", "btn-close-modal");
+    buttonElement.setAttribute("type", "button");
+    buttonElement.setAttribute("value", "Fermer");
+
+    divElement.appendChild(buttonElement);
+
+    // Put the div element into the modal body
+    modalBodyElement.appendChild(divElement);
+
+    closeModalCrossElement.addEventListener("click", closeConfirmModal);
+
+    const btnCloseModalElement = document.querySelector(".btn-close-modal");
+    btnCloseModalElement.addEventListener("click", closeConfirmModal);
+  }
+
+  // ==================================
+  // === Modal confirmation closing ===
+  // ==================================
+
+  function closeConfirmModal() {
+    // Select and remove the p Element
+    modalBodyElement.querySelector("p").remove();
+    // Select and remove the button Element
+    modalBodyElement.querySelector(".btn-close-modal").remove();
+    // Select and remove the div Element
+    modalBodyElement.querySelector(".modal-confirm").remove();
+    // close the modal
+    modalbg.style.display = "none";
+  }
+
+  // ========================================
+  // === Get the form datas ===
+  // === and display them in the console ====
+  // ========================================
+
+  function logDatas() {
+    const datas = document.querySelectorAll('[data-validate]');
+    datas.forEach(data => {
+      console.log(`${data.name} : ${data.value}`);
+    })
+  }
+
+});
